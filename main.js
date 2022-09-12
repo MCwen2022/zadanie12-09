0823d2659c0699216c3d4ae56459f87fd5829d5e
MCwen2022 <112862536+MCwen2022@users.noreply.github.com>
2022-09-12 13:09:49 +0200

    Najnowsiejsza wersja

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 number1 = document.querySelector(".num1");
 number2 = document.querySelector(".num2");
-mnoz = document.querySelector(".mnoz");
-dziel = document.querySelector(".dziel");
-dod = document.querySelector(".dod");
-ode = document.querySelector(".odej");
-sec = document.querySelector("section");
+times = document.querySelector(".mnoz");
+divide = document.querySelector(".dziel");
+add = document.querySelector(".dod");
+subtract = document.querySelector(".odej");
+wynik = document.querySelector("section");
 
-mnoz.addEventListener('click', function(evt) {
+times.addEventListener('click', function(evt) {
     evt.preventDefault();
-    sec.innerHTML = (parseInt(number1.value) * parseInt(number2.value));
+    wynik.innerHTML = (parseInt(number1.value) * parseInt(number2.value));
 });
 
-dziel.addEventListener('click', function(evt) {
+divide.addEventListener('click', function(evt) {
     evt.preventDefault();
     if(parseInt(number2.value) != 0){
-        sec.innerHTML = (parseInt(number1.value) / parseInt(number2.value));
+        wynik.innerHTML = (parseInt(number1.value) / parseInt(number2.value));
     }else{
-        sec.innerHTML = ("Nie dziel przez zero");
+        wynik.innerHTML = ("Nie dziel przez zero");
     }
     
 });
 
-dod.addEventListener('click', function(evt) {
+add.addEventListener('click', function(evt) {
     evt.preventDefault();
-    sec.innerHTML = (parseInt(number1.value) + parseInt(number2.value));
+    wynik.innerHTML = (parseInt(number1.value) + parseInt(number2.value));
 });
 
-ode.addEventListener('click', function(evt) {
+subtract.addEventListener('click', function(evt) {
     evt.preventDefault();
-    sec.innerHTML = (parseInt(number1.value) - parseInt(number2.value));
+    wynik.innerHTML = (parseInt(number1.value) - parseInt(number2.value));
 });
 
