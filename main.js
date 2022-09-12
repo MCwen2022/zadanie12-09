b919acff4eed57afa252ef19531256273f76bb6b
MCwen2022 <112862536+MCwen2022@users.noreply.github.com>
2022-09-12 13:15:21 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
 number1 = document.querySelector(".num1");
 number2 = document.querySelector(".num2");
+
 times = document.querySelector(".mnoz");
 divide = document.querySelector(".dziel");
 add = document.querySelector(".dod");
 subtract = document.querySelector(".odej");
-wynik = document.querySelector("section");
 
+result = document.querySelector("section");
+
+// mnożenie
 times.addEventListener('click', function(evt) {
     evt.preventDefault();
-    wynik.innerHTML = (parseInt(number1.value) * parseInt(number2.value));
+    result.innerHTML = (parseInt(number1.value) * parseInt(number2.value));
 });
 
+// dzielenie
 divide.addEventListener('click', function(evt) {
     evt.preventDefault();
     if(parseInt(number2.value) != 0){
-        wynik.innerHTML = (parseInt(number1.value) / parseInt(number2.value));
+        result.innerHTML = (parseInt(number1.value) / parseInt(number2.value));
     }else{
-        wynik.innerHTML = ("Nie dziel przez zero");
+        result.innerHTML = ("Nie dziel przez zero");
     }
-    
 });
 
+// dodawanie
 add.addEventListener('click', function(evt) {
     evt.preventDefault();
-    wynik.innerHTML = (parseInt(number1.value) + parseInt(number2.value));
+    result.innerHTML = (parseInt(number1.value) + parseInt(number2.value));
 });
 
+// odejmowanie
 subtract.addEventListener('click', function(evt) {
     evt.preventDefault();
-    wynik.innerHTML = (parseInt(number1.value) - parseInt(number2.value));
+    result.innerHTML = (parseInt(number1.value) - parseInt(number2.value));
 });
 
